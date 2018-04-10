d2d0d378ece25488d8fbbe916dc15dd0744c67fd
_g3r0_ <germanlm93@gmail.com>
2018-04-09 20:26:37 -0500

    Update and rename __init__.py to examples.py

--- examples.py
+++ examples.py
@@ -1,6 +1,9 @@
+""" Examples """
+
 from src.pdf_watermark import PDF
 
-""" Example """	
 p = PDF('C://tmp/a.pdf', 'C://tmp/o.pdf', 'C://tmp/img.jpg')
 p.watermark_all_pages()
 
+p = PDF("C://tmp/a.pdf", "C://tmp/w.pdf", "C://tmp/img.jpg")
+pdf.watermark_all_pages()
